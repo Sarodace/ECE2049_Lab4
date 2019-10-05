0f32bc18f7eb6e54397be4678856e6e5793a766b
Jonathan Ferreira <jgferreira@wpi.edu>
2019-10-05 18:02:44 -0400

    Baseline code
    Just copying over the old code from lab 3 to have some code to work off
    of

---
+++ main.cpp
@@ -0,0 +1,413 @@
+/**************** ECE2049 Lab 3 ******************/
+/*************  25 September 2019 ****************/
+/******  Benjamin Ward, Jonathan Ferreira ********/
+/*************************************************/
+
+#include <msp430.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <inc/peripherals.h>
+#include <inc/ADC.h>
+#include <main.h>
+#include <inc/time.h>
+
+// States utilized in main program state machine
+typedef enum State
+{
+    DISPLAY, WAIT_FOR_TRANSITION, BEGIN_EDIT, DRAW_EDIT, EDIT
+} State;
+
+// States used in display screen
+typedef enum DisplayState
+{
+    DATE, TIME, TEMP_C, TEMP_F
+} DisplayState;
+
+// States used in edit screen
+typedef enum EditState
+{
+    MONTH, DAY, HOUR, MINUTE, SECOND
+} EditState;
+
+// Defined in main.h
+uint32_t globalClock = monthToSeconds(0) + monthToSeconds(1) + monthToSeconds(2)
+        + monthToSeconds(3) + monthToSeconds(4) + monthToSeconds(5)
+        + dayToSeconds(4);
+
+uint32_t globalCounter = 0;
+
+// Function prototypes
+void drawDate();
+void drawTime();
+void drawTempC(ADC*);
+void drawTempF(ADC*);
+void configButtons();
+unsigned char getButtonState();
+void drawDateTime(Date, Time);
+void drawUnderline(EditState);
+
+// Program entry point
+void main(void)
+{
+    // Main loop state
+    uint32_t startCounter, deltaCounter;
+    State state = DISPLAY;
+    DisplayState displayState = DATE;
+    EditState editState = MONTH;
+
+    uint8_t buttonPressed, lastButtonPressed;
+    int32_t startPot, deltaPot, potMonthIncrement = 200, potMiscIncrement = 40;
+
+    Date editDate, editStartDate;
+    Time editTime, editStartTime;
+
+    WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
+
+    // Set up and configure peripherals and I/O
+    initLeds();
+    configDisplay();
+    configButtons();
+    configKeypad();
+
+    ADC* adc = new ADC();
+
+    // Main loop
+    while (1)
+    {
+        buttonPressed = getButtonState();
+
+        switch (state)
+        {
+        case DISPLAY:
+            // Save initial time
+            startCounter = globalCounter;
+            // Draw parameters
+            switch (displayState)
+            {
+            case DATE:
+                drawDate();
+                break;
+            case TIME:
+                drawTime();
+                break;
+            case TEMP_C:
+                drawTempC(adc);
+                break;
+            case TEMP_F:
+                drawTempF(adc);
+                break;
+            }
+            state = WAIT_FOR_TRANSITION;
+            break;
+        case WAIT_FOR_TRANSITION:
+            // If the left lab board button is pressed, go to edit mode
+            if (buttonPressed & BIT0)
+            {
+                state = BEGIN_EDIT;
+            }
+            // Elapsed time
+            deltaCounter = globalCounter - startCounter;
+            // Display next parameter if 2 seconds have elapsed
+            if (deltaCounter >= 2)
+            {
+                switch (displayState)
+                {
+                case DATE:
+                    displayState = TIME;
+                    break;
+                case TIME:
+                    displayState = TEMP_C;
+                    break;
+                case TEMP_C:
+                    displayState = TEMP_F;
+                    break;
+                case TEMP_F:
+                    displayState = DATE;
+                    break;
+                }
+                state = DISPLAY;
+            }
+            break;
+
+        case BEGIN_EDIT:
+            // Declare variables that'll store edited values
+            editDate = currentDate();
+            editStartDate = currentDate();
+            editTime = currentTime();
+            editStartTime = currentTime();
+
+            //Save initial value of the potentiometer
+            startPot = adc->getCurrentPot();
+
+            state = DRAW_EDIT;
+            break;
+        case DRAW_EDIT:
+            // Draw current date and time
+            drawDateTime(editDate, editTime);
+            // Draw line under currently selected time division
+            drawUnderline(editState);
+            state = EDIT;
+            break;
+        case EDIT:
+            // If new button is pressed
+            if (buttonPressed != lastButtonPressed)
+            {
+                // Left button, go to next time division
+                if (buttonPressed & BIT0)
+                {
+                    editState = (EditState) ((editState + 1) % 5);
+                    // Reset potentiometer input every time we transition between edit states
+                    startPot = adc->getCurrentPot();
+                    editStartDate = editDate;
+                    editStartTime = editTime;
+                    state = DRAW_EDIT;
+                }
+                // Right button, exit edit state
+                if (buttonPressed & BIT1)
+                {
+                    // Save time to global time, then switch back to DISPLAY
+                    // and set display state to DATE
+                    globalClock = dateToSeconds(editDate)
+                            + timeToSeconds(editTime);
+                    state = DISPLAY;
+                    displayState = DATE;
+                    break;
+                }
+            }
+            else
+            {
+                // Difference in potentiometer position
+                deltaPot = adc->getCurrentPot() - startPot;
+                // Scroll mode
+                switch (editState)
+                {
+                case MONTH:     // Edit month
+                    uint32_t month = deltaNumber(editStartDate.month, 0, 11, deltaPot, potMonthIncrement);
+                    if (month != editDate.month)
+                    {
+                        editDate.month = month;
+                        state = DRAW_EDIT;
+                    }
+                    break;
+                case DAY:     // Edit day
+                    uint32_t day = deltaNumber(editStartDate.day, 1, monthDayCounts[editDate.month], deltaPot, potMiscIncrement);
+                    if (day != editDate.day)
+                    {
+                        editDate.day = day;
+                        state = DRAW_EDIT;
+                    }
+                    break;
+                case HOUR:   // Edit hour
+                    uint32_t hour = deltaNumber(editStartTime.hours, 0, 23, deltaPot, potMiscIncrement);
+                    if (hour != editTime.hours)
+                    {
+                        editTime.hours = hour;
+                        state = DRAW_EDIT;
+                    }
+                    break;
+                case MINUTE: // Edit minute
+                    uint32_t minutes = deltaNumber(editStartTime.minutes, 0, 59, deltaPot, potMiscIncrement);
+                    if (minutes != editTime.minutes)
+                    {
+                        editTime.minutes = minutes;
+                        state = DRAW_EDIT;
+                    }
+                    break;
+                case SECOND: // Edit month
+                    uint32_t seconds = deltaNumber(editStartTime.seconds, 0, 59, deltaPot, potMiscIncrement);
+                    if (seconds != editTime.seconds)
+                    {
+                        editTime.seconds = seconds;
+                        state = DRAW_EDIT;
+                    }
+                    break;
+                }
+            }
+            break;
+        }
+        lastButtonPressed = buttonPressed;
+    }
+}
+
+// Draw date
+void drawDate()
+{
+    Graphics_clearDisplay(&g_sContext);
+
+    Date date = currentDate();
+    const char* month = monthNames[date.month];
+
+    char dBuffer[6];
+    dBuffer[0] = month[0];
+    dBuffer[1] = month[1];
+    dBuffer[2] = month[2];
+    dBuffer[3] = ' ';
+    dBuffer[4] = date.day / 10 % 10 + '0';
+    dBuffer[5] = date.day % 10 + '0';
+
+    Graphics_drawStringCentered(&g_sContext, (uint8_t*) dBuffer, 6, 48, 25,
+    TRANSPARENT_TEXT);
+
+    Graphics_Rectangle box = { .xMin = 5, .xMax = 91, .yMin = 5, .yMax = 91 };
+    Graphics_drawRectangle(&g_sContext, &box);
+    Graphics_flushBuffer(&g_sContext);
+}
+
+// Draw time
+void drawTime()
+{
+    Graphics_clearDisplay(&g_sContext);
+
+    Time time = currentTime();
+
+    char dBuffer[8];
+    dBuffer[0] = time.hours / 10 % 10 + '0';
+    dBuffer[1] = time.hours % 10 + '0';
+    dBuffer[2] = ':';
+    dBuffer[3] = time.minutes / 10 % 10 + '0';
+    dBuffer[4] = time.minutes % 10 + '0';
+    dBuffer[5] = ':';
+    dBuffer[6] = time.seconds / 10 % 10 + '0';
+    dBuffer[7] = time.seconds % 10 + '0';
+
+    Graphics_drawStringCentered(&g_sContext, (uint8_t*) dBuffer, 8, 48, 25,
+    TRANSPARENT_TEXT);
+
+    Graphics_Rectangle box = { .xMin = 5, .xMax = 91, .yMin = 5, .yMax = 91 };
+    Graphics_drawRectangle(&g_sContext, &box);
+    Graphics_flushBuffer(&g_sContext);
+}
+
+// Draw temperature in Celsius
+void drawTempC(ADC* adc)
+{
+    Graphics_clearDisplay(&g_sContext);
+
+    float tempC = adc->getCurrentTempC();
+
+    char dBuffer[6];
+    dBuffer[0] = (uint8_t) tempC / 10 % 10 + '0';
+    dBuffer[1] = (uint8_t) tempC % 10 + '0';
+    dBuffer[2] = '.';
+    dBuffer[3] = (uint8_t) (tempC * 10) % 10 + '0';
+    dBuffer[4] = ' ';
+    dBuffer[5] = 'C';
+
+    Graphics_drawStringCentered(&g_sContext, (uint8_t*) dBuffer, 6, 48, 25,
+    TRANSPARENT_TEXT);
+
+    Graphics_Rectangle box = { .xMin = 5, .xMax = 91, .yMin = 5, .yMax = 91 };
+    Graphics_drawRectangle(&g_sContext, &box);
+    Graphics_flushBuffer(&g_sContext);
+}
+
+// Draw temperature in Farenheit
+void drawTempF(ADC* adc)
+{
+    Graphics_clearDisplay(&g_sContext);
+
+    float tempF = adc->getCurrentTempF();
+
+    char dBuffer[6];
+    dBuffer[0] = (uint8_t) tempF / 10 % 10 + '0';
+    dBuffer[1] = (uint8_t) tempF % 10 + '0';
+    dBuffer[2] = '.';
+    dBuffer[3] = (uint8_t) (tempF * 10) % 10 + '0';
+    dBuffer[4] = ' ';
+    dBuffer[5] = 'F';
+
+    Graphics_drawStringCentered(&g_sContext, (uint8_t*) dBuffer, 6, 48, 25,
+    TRANSPARENT_TEXT);
+
+    Graphics_Rectangle box = { .xMin = 5, .xMax = 91, .yMin = 5, .yMax = 91 };
+    Graphics_drawRectangle(&g_sContext, &box);
+    Graphics_flushBuffer(&g_sContext);
+}
+
+// Draw date and time
+void drawDateTime(Date date, Time time)
+{
+    Graphics_clearDisplay(&g_sContext);
+
+    const char* month = monthNames[date.month];
+
+    char dBuffer[6];
+    dBuffer[0] = month[0];
+    dBuffer[1] = month[1];
+    dBuffer[2] = month[2];
+    dBuffer[3] = ' ';
+    dBuffer[4] = date.day / 10 % 10 + '0';
+    dBuffer[5] = date.day % 10 + '0';
+
+    char tBuffer[8];
+    tBuffer[0] = time.hours / 10 % 10 + '0';
+    tBuffer[1] = time.hours % 10 + '0';
+    tBuffer[2] = ':';
+    tBuffer[3] = time.minutes / 10 % 10 + '0';
+    tBuffer[4] = time.minutes % 10 + '0';
+    tBuffer[5] = ':';
+    tBuffer[6] = time.seconds / 10 % 10 + '0';
+    tBuffer[7] = time.seconds % 10 + '0';
+
+    Graphics_drawStringCentered(&g_sContext, (uint8_t*) dBuffer, 6, 48, 25,
+    TRANSPARENT_TEXT);
+    Graphics_drawStringCentered(&g_sContext, (uint8_t*) tBuffer, 8, 48, 55,
+    TRANSPARENT_TEXT);
+
+    Graphics_Rectangle box = { .xMin = 5, .xMax = 91, .yMin = 5, .yMax = 91 };
+    Graphics_drawRectangle(&g_sContext, &box);
+    Graphics_flushBuffer(&g_sContext);
+}
+
+// Draw selection line
+void drawUnderline(EditState state)
+{
+    switch (state)
+    {
+    case MONTH:
+        Graphics_drawLineH(&g_sContext, 30, 46, 32);
+        break;
+    case DAY:
+        Graphics_drawLineH(&g_sContext, 52, 66, 32);
+        break;
+    case HOUR:
+        Graphics_drawLineH(&g_sContext, 23, 35, 62);
+        break;
+    case MINUTE:
+        Graphics_drawLineH(&g_sContext, 41, 53, 62);
+        break;
+    case SECOND:
+        Graphics_drawLineH(&g_sContext, 59, 71, 62);
+        break;
+    }
+    Graphics_flushBuffer(&g_sContext);
+}
+
+// Configure lab board buttons
+void configButtons()
+{
+    // Left Button
+    P2SEL &= ~(BIT1); // Select pin for DI/O
+    P2DIR &= ~(BIT1); // Set pin as input
+    P2REN |= (BIT1); // Enable input resistor
+    P2OUT |= (BIT1); // Set resistor to pull up
+
+    // Right Button
+    P1SEL &= ~(BIT1); // Select pin for DI/O
+    P1DIR &= ~(BIT1); // Set pin as input
+    P1REN |= (BIT1); // Enable input resistor
+    P1OUT |= (BIT1); // Set resistor to pull up
+}
+
+// Get the state of the lab board buttons
+unsigned char getButtonState()
+{
+    unsigned char ret = 0x00;
+    // P2.1
+    if (~P2IN & BIT1)
+        ret |= BIT0; // Left button (S1)
+    // P1.1
+    if (~P1IN & BIT1)
+        ret |= BIT1; // Right button (S2)
+    return ret;
+}
---
+++ src/ADC.cpp
@@ -0,0 +1,115 @@
+/*
+ * TemperatureSensor.cpp
+ *
+ *  Created on: Sep 25, 2019
+ *      Author: blward
+ */
+
+#include <inc/ADC.h>
+#include <main.h>
+#include <msp430.h>
+#include <numeric>
+
+// Global variables
+uint32_t ADC::rawTempReadings[10];
+uint32_t ADC::averagedTempReadings[30];
+uint32_t ADC::rawPotReading;
+uint32_t ADC::readingIndex;
+
+ADC::ADC()
+{
+    // Using the chip-specific calibration data stored in device registers, calculate
+    // the ADC reading to degrees Celsius conversion
+    tempCal30C = *((unsigned int *)0x1A1A);
+    tempCal85C = *((unsigned int *)0x1A1C);
+    this->degCPerBit = (85.0F - 30.0F)/(float)(tempCal85C - tempCal30C);
+
+    REFCTL0 &= ~REFMSTR; // Reset REFMSTR to hand over control of internal reference
+
+    // Potentiometer
+    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON | ADC12MSC; // Start ADC with 1.5V internal reference
+
+    // Temperature Sensor
+    ADC12CTL1 = ADC12SHP | ADC12CONSEQ_1 | ADC12CSTARTADD_0;
+
+    // Using ADC12MEM1 for conversion result, so enable interrupt for MEM1
+    ADC12IE = (BIT0 | BIT1);
+
+    ADC12MCTL0 = ADC12SREF_0 | ADC12INCH_0; // Use 3.3V ref (VCC)
+    ADC12MCTL1 = ADC12SREF_1 | ADC12INCH_10 | ADC12EOS; // Use 1.5V ref
+    P6SEL |= BIT0; // Set P6.0 to function mode to enable use of A0
+
+    // Configure Timer A2
+    TA2CTL = TASSEL_1 + ID_3 + MC_1;
+    TA2CCR0 = 4095; // = ~1 second
+    TA2CCTL0 = CCIE; // TA2CCR0 interrupt enabled
+
+    for (int i = 0; i < 5; i++) {
+        __delay_cycles(10000);
+        ADC12CTL0 |= (ADC12SC | ADC12ENC); // Start conversion
+
+        // Poll busy bit waiting for the first conversion to complete
+        while (ADC12CTL1 & ADC12BUSY)
+            __no_operation();
+    }
+
+    // Collect temperature readings
+    for(int i = 0; i < 10; i++)
+        rawTempReadings[i] = ADC12MEM1 & 0x0FFF;
+
+    // Average the temperature readings
+    for(int i = 0; i < 30; i++)
+        averagedTempReadings[i] = ADC12MEM1 & 0x0FFF;
+
+    _BIS_SR(GIE); // Global interrupt enable
+}
+
+ADC::~ADC()
+{
+    // Not necessary, ADC instance will not be deconstructed
+}
+
+// Get current temperature in Farenheit
+float ADC::getCurrentTempF()
+{
+    return getCurrentTempC() * 9.0F/5.0F + 32.0F;
+}
+
+// Get current temperature in Celsius
+float ADC::getCurrentTempC()
+{
+    return (float)(((int32_t)averagedTempReadings[readingIndex % 30]) - tempCal30C) * degCPerBit + 30.0F;
+}
+
+// Get current potentiometer reading
+uint32_t ADC::getCurrentPot() {
+    return rawPotReading;
+}
+
+// Configure the Timer A2 ISR
+#pragma vector=TIMER2_A0_VECTOR
+__interrupt void ADC::TIMERA2ISR()
+{
+    globalCounter++;
+    globalClock++;
+    ADC12CTL0 |= (ADC12SC | ADC12ENC); // Start conversion
+}
+
+// Configure the temperature and potentiometer ISR
+#pragma vector=ADC12_VECTOR
+__interrupt void ADC::ADC12ISR() {
+    // Move results to static
+    rawPotReading = ADC12MEM0 & 0x0FFF;
+    rawTempReadings[readingIndex % 10] = ADC12MEM1 & 0x0FFF;
+
+    int i, sum  = 0;
+    for(i = 0; i < 10; i++)
+    {
+        sum += rawTempReadings[i];
+    }
+
+    averagedTempReadings[readingIndex % 30] = sum / 10;
+
+    readingIndex++;
+}
+
